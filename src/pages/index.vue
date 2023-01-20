0e5439c358f116b9f27903754fe47da02c6d70b0
vhikZhangchao <zhangchao564265135@hotmail.com>
2023-01-20 19:23:17 +0800

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <a-typography :style="{ marginTop: '-40px' }">
+      <a-typography-title>
+        Arco Uno Basic Template
+      </a-typography-title>
+      <a-space>
+        <a-button type="primary">
+          Primary
+        </a-button>
+        <a-button>Secondary</a-button>
+        <a-button type="dashed">
+          Dashed
+        </a-button>
+        <a-button type="outline">
+          Outline
+        </a-button>
+        <a-button type="text">
+          Text
+        </a-button>
+      </a-space>
+    </a-typography>
+  </div>
+</template>
