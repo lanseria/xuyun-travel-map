1c5ba9391193954fe35e6ef0e700df341370f840
lanseria <zhangchao564265135@hotmail.com>
2023-02-10 20:18:42 +0800

    chore

--- src/composables/constants.ts
+++ src/composables/constants.ts
@@ -1,9 +1,5 @@
 // import { osmStyle } from './mapStyle'
 
-const MapBoxStylePrefix = 'mapbox://styles'
-
-export const MapboxAccessToken = 'pk.eyJ1IjoibGFuc2VyaWEiLCJhIjoiY2tzNDE4MDI0MHg5ZjJvcndtZzF4YTB6aCJ9.5k-y1Bx3km5MAOp4KVpb9g'
-
 export const MAP_PLACE_SOURCE = 'MapPlaceSource'
 export const MAP_PLACE_LAYER_POINT = 'MapPlaceLayerPoint'
 export const MAP_PLACE_LAYER_LINESTRING_BG = 'MAP_PLACE_LAYER_LINESTRING_BG'
@@ -11,26 +7,23 @@ export const MAP_PLACE_LAYER_LINESTRING_DASHED = 'MAP_PLACE_LAYER_LINESTRING_DAS
 
 export const MAP_PLACE_LAYER_BBOX = 'MapPlaceLayerBbox'
 
+export const MapboxAccessToken = 'pk.eyJ1IjoibGFuc2VyaWEiLCJhIjoiY2tzNDE4MDI0MHg5ZjJvcndtZzF4YTB6aCJ9.5k-y1Bx3km5MAOp4KVpb9g'
+
 export const LayerStyleList = [
-  // {
-  //   name: 'osm',
-  //   value: 'osm',
-  //   style: osmStyle,
-  // },
   {
     name: 'streets',
     value: 'streets',
-    style: `${MapBoxStylePrefix}/lanseria/cld5ur36v000301lngmbtq0qh`,
+    style: 'mapbox://styles/lanseria/cld5ur36v000301lngmbtq0qh',
   },
   {
     name: 'Satellite Streets',
     value: 'satellite_streets',
-    style: `${MapBoxStylePrefix}/lanseria/cldecwoux001t01pk90yx1jj3`,
+    style: 'mapbox://styles/lanseria/cldecwoux001t01pk90yx1jj3',
   },
   {
     name: 'Monochrome',
     value: 'monochrome',
-    style: `${MapBoxStylePrefix}/lanseria/cldwdod87000e01pcn2ezak1n`,
+    style: 'mapbox://styles/lanseria/cldwdod87000e01pcn2ezak1n',
   },
 ]
 
